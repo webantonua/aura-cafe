6a365404d6df8101118afa49fade2c5b1e373f35
Anttoha <webantonwebanton@gmail.com>
2026-06-23 16:08:01 +0200

    setting project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='text-yellow font-black'>
+      Hello World!
+    </div>
+  )
+}
+
+export default App
